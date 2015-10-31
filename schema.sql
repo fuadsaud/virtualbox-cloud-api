1c7f17f30858b364be8447fe6d06da3fc806faf0
Luiz Cunha <felipe.silvacunha@gmail.com>
2015-10-31 19:59:02 -0200

    Adjusted sql syntax

--- schema.sql
+++ schema.sql
@@ -1,2 +1,2 @@
-CREATE TABLE vms (id INTEGER PRIMARY KEY, name TEXT, os_id)
-CREATE TABLE oss (id INTEGER PRIMARY KEY, name TEXT, path TEXT, vboxid TEXT)
+CREATE TABLE vms (id INTEGER PRIMARY KEY, name TEXT, os_id);
+CREATE TABLE oss (id INTEGER PRIMARY KEY, name TEXT, path TEXT, vboxid TEXT);
