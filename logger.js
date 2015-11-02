dc850f67594cbd16e579697d899595fa6c1865dc
Luiz Cunha <felipe.silvacunha@gmail.com>
2015-11-02 20:24:17 -0200

    Added debug log entry

--- logger.js
+++ logger.js
@@ -1,5 +1,6 @@
 module.exports =  {
   logError: function (msg) { this.log('[ERROR] ' + msg) },
   logInfo:  function (msg) { this.log('[INFO] ' + msg) },
+  logDebug: function (msg) { this.log('[DEBUG] ' + msg) },
   log: console.log
 }
